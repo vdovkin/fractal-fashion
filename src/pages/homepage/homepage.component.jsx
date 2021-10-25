39e9983b26734973aae3d49afaaad3539058fb9f
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-10-25 23:07:11 +0300

    basic structure of home page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <div>
+                Главный банер - слайдер
+            </div>
+
+            <div>
+                Категории
+            </div>
+
+            <div>
+                Новинки
+            </div>
+
+            <div>
+                Распродажа
+            </div>
+
+            <div>
+                Блог
+            </div>
+
+        </div>
+    )
+}
+
+export default HomePage;
